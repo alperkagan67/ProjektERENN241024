ba9275431a37eec0e1a9891b4387af57f1206e5c
juliandete <julian@determeyser.info>
2024-10-25 10:55:01 +0200

    footer_moin

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,7 +1,36 @@
-import styles from "./Footer.module.css";
+// src/components/Footer.js
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGavel, faEnvelope, faShieldAlt } from '@fortawesome/free-solid-svg-icons';
+import './Footer.module.css';
+
 
 function Footer() {
-  return <footer className={styles.container}>This is my footer</footer>;
+  return (
+    <Navbar
+      className="footer-navbar mt-5"
+      style={{
+        background: 'linear-gradient(135deg, #1a1a2e, #16213e)',
+        padding: '20px',
+        color: '#fff'
+      }}
+    >
+      <Container className="d-flex justify-content-center">
+        <Nav className="footer-nav">
+          <Nav.Link href="#impressum" className="footer-link mx-3">
+            <FontAwesomeIcon icon={faGavel} /> Impressum
+          </Nav.Link>
+          <Nav.Link href="#kontakt" className="footer-link mx-3">
+            <FontAwesomeIcon icon={faEnvelope} /> Kontakt
+          </Nav.Link>
+          <Nav.Link href="#datenschutz" className="footer-link mx-3">
+            <FontAwesomeIcon icon={faShieldAlt} /> Datenschutz
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Footer;
